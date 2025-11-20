476cc14db5cc3748325cf895aeccaf1d57f47f6b
Vhinil-O <de.sanchezpadilla@ugto.mx>
2025-11-20 15:12:36 -0600

    correccion

--- backend/app/models/appointment.py
+++ backend/app/models/appointment.py
@@ -4,7 +4,7 @@ from sqlmodel import Field, SQLModel
 
 #Modelo para CITAS
 class appointments (SQLModel, table=True):
-    id_appoimtment : int | None = Field(default=None, primary_key=True)
+    id_appointment : int | None = Field(default=None, primary_key=True)
     doctor_id : int = Field(foreign_key="doctors.id_doctor")
     patient_id : int = Field(foreign_key="patients.id_patient")
     fecha : datetime
