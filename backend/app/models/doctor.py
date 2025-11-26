3ed77c9039118f0ba04afe4fe88bf2f8bfca3b0b
Edgar <edj.medinagonzalez@ugto.mx>
2025-11-25 18:27:38 -0600

    [ADDED]: Se agregaron los modelos, por alguna razon se actualizo el fronted

--- backend/app/models/doctor.py
+++ backend/app/models/doctor.py
@@ -1,12 +1,24 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
-#Modelo para el DOCTOR
-class doctors(SQLModel, table=True) :
-    id_doctor: int | None = Field(default=None, primary_key=True)
+#voy hacer varios comentarios para entender lo que estoy haciendo
+
+#estructura dividida en 4 partes: Base, Crear, Leer y Tabla.
+##pq?
+#Seguridad: Para no pedir el id cuando creas un doctor (eso es automático).
+#Orden: Para separar lo que el usuario escribe de lo que la base de datos guarda.
+
+class DoctorBase(SQLModel): #la base, es decir datos compartidos
     nombre: str
     especialidad: str
 
-    user_id: int = Field(default=None, foreign_key="users.id_user")
+class DoctorCreate(DoctorBase): #hereda doctorBase,es identico a la funcion anterior
+    pass #significa literalmente no hagas nada
 
 
+class DoctorRead(DoctorBase):
+    id_doctor: int # Aquí sí mostramos el ID para que el frontend sepa cuál es.
+
+class doctors(DoctorBase, table=True): #table=True le dice a SQLModel: "Crea esto en SQLite".
+    id_doctor: int | None = Field(default=None, primary_key=True)
+    user_id: int | None = Field(default=None, foreign_key="users.id_user") #La conexión con el usuario (Login)
--- backend/app/models/patient.py
+++ backend/app/models/patient.py
@@ -1,9 +1,22 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
-#Modelo para PACIENTE
-class patients(SQLModel, table=True) :
-    id_patient : int | None = Field(default=None, primary_key=True)
-    nombre : str
-    telefono : str
-    email : str
+#el paciente no tien user id, no inicia sesion , es solo un registro medico
+
+# 1. LA BASE: Datos comunes del paciente
+class PatientBase(SQLModel):
+    nombre: str
+    telefono: str
+    email: str
+
+
+class PatientCreate(PatientBase):#ESQUEMA CREAR: Lo que recibimos del formulario
+    pass
+
+
+class PatientRead(PatientBase):#ESQUEMA LEER: Lo que entregamos al consultar
+    id_patient: int
+
+
+class patients(PatientBase, table=True):#LA TABLA: La estructura real en disco
+    id_patient: int | None = Field(default=None, primary_key=True)
